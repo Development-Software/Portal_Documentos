8422a8ac0fbd9131aca0bb2a208b1659ed232fa8
Omar Martinez <omar.martinez@ula.edu.mx>
2021-03-10 23:22:09 -0600

    Update Inicial

---
+++ Portal_Documentos/App_Code/CASP.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Web.UI;
+using System.IO;
+using System.Net;
+
+/// <summary>
+/// Descripción breve de CASP
+/// </summary>
+public class CASP
+{
+    /** 
+     * Authenticates a user with the given login and validation pages. After authentication 
+     * the user's browser is redirected to the original page. 
+     */
+
+    public static String Authenticate(String LoginURL, String ValidateURL, Page Page)
+    {
+        return Authenticate(LoginURL, ValidateURL, Page, Page.Request.Url.AbsoluteUri.Split('?')[0]);
+    }
+
+    /** 
+     * Authenticates a user with the given login and validation pages. After authentication 
+     * the user's browser is redirected to the location given as the service URL. 
+     */
+
+    public static String Authenticate(String LoginURL, String ValidateURL, Page Page, String ServiceURL)
+    {
+        if (Page.Session["CASNetworkID"] != null) // user already logged in 
+            return Page.Session["CASNetworkID"].ToString();
+        else // user hasn't logged in 
+        {
+            if (Page.Request.QueryString["ticket"] != null) // ticket received 
+            {
+                try // read ticket and request validation 
+                {
+                    StreamReader Reader = new StreamReader(new WebClient().OpenRead(ValidateURL + "?ticket=" + Page.Request.QueryString["ticket"] + "&service=" + ServiceURL));
+
+                    if ("yes".Equals(Reader.ReadLine())) // ticket validated 
+                    {
+                        // store network id in sesssion, return value 
+
+                        return (String)(Page.Session["CASNetworkID"] = Reader.ReadLine());
+
+                    }
+                }
+                catch (WebException) { }
+            }
+
+            // ticket was invalid, or didn't exist, so request ticket 
+
+            Page.Response.Redirect(LoginURL + "?service=" + ServiceURL, true);
+            return null;
+        }
+    }
+}
---
+++ Portal_Documentos/App_Code/Data.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Collections;
+using System.Data.SqlClient;
+
+namespace applyWeb.Data
+{
+    public class Data
+    {
+        private string mstrConnectionString;
+        public Data(string pstrConnectionString)
+        {
+            mstrConnectionString = pstrConnectionString;
+        }
+
+        public int ExecuteInsertSP(string pstrName, ArrayList parrParameters)
+        {
+            using (SqlConnection objCnn = new SqlConnection(mstrConnectionString))
+            {
+
+                //SqlConnection objCnn = new SqlConnection(mstrConnectionString);
+                SqlCommand objCmd = new SqlCommand();
+
+                try
+                {
+                    objCnn.Open();
+                    objCmd.Connection = objCnn;
+                    objCmd.CommandType = CommandType.StoredProcedure;
+                    objCmd.CommandText = pstrName;
+
+                    foreach (Parametro objParam in parrParameters)
+                    {
+                        SqlParameter objNewParam = new SqlParameter(objParam.Nombre, objParam.Valor);
+                        objCmd.Parameters.Add(objNewParam);
+                    }
+
+                    return objCmd.ExecuteNonQuery();
+
+                }
+                catch (Exception e)
+                {
+                    return 0;
+                }
+                finally
+                {
+                    objCnn.Close();
+                    objCnn.Dispose();
+                    objCmd.Dispose();
+                }
+            }
+        }
+
+        public IDataReader ExecuteReader(string pstrName, ArrayList parrParameters)
+        {
+            //using (SqlConnection objCnn = new SqlConnection(mstrConnectionString))
+            //{
+            SqlConnection objCnn = new SqlConnection(mstrConnectionString);
+            SqlCommand objCmd = new SqlCommand();
+            SqlDataAdapter objDA = new SqlDataAdapter();
+
+
+            try
+            {
+                objCnn.Open();
+                objCmd.Connection = objCnn;
+                objCmd.CommandType = CommandType.StoredProcedure;
+                objCmd.CommandText = pstrName;
+
+                foreach (Parametro objParam in parrParameters)
+                {
+                    SqlParameter objNewParam = new SqlParameter(objParam.Nombre, objParam.Valor);
+                    objCmd.Parameters.Add(objNewParam);
+                }
+
+                return objCmd.ExecuteReader();
+            }
+            catch (Exception es)
+            {
+                throw new Exception(es.Message);
+            }
+            finally
+            {
+                //objCnn.Close();
+                //objCnn = null;
+                //objCmd = null;
+                //objDA = null;
+            }
+            //}
+        }
+
+        public DataSet ExecuteSP(string pstrName, ArrayList parrParameters)
+        {
+            using (SqlConnection objCnn = new SqlConnection(mstrConnectionString))
+            {
+                SqlCommand objCmd = new SqlCommand();
+                SqlDataAdapter objDA = new SqlDataAdapter();
+                DataSet dsReturn = new DataSet();
+
+                try
+                {
+                    objCnn.Open();
+                    objCmd.Connection = objCnn;
+                    objCmd.CommandType = CommandType.StoredProcedure;
+                    objCmd.CommandText = pstrName;
+
+                    foreach (Parametro objParam in parrParameters)
+                    {
+                        SqlParameter objNewParam = new SqlParameter(objParam.Nombre, objParam.Valor);
+                        objCmd.Parameters.Add(objNewParam);
+                    }
+
+                    objDA.SelectCommand = objCmd;
+
+                    objDA.Fill(dsReturn);
+                    return dsReturn;
+                }
+                catch (Exception es)
+                {
+                    throw new Exception(es.Message);
+                }
+                finally
+                {
+                    objCnn.Close();
+                    objCnn.Dispose();
+                    objCmd.Dispose();
+                    objDA.Dispose();
+                }
+            }
+        }
+    }
+
+    public class Parametro
+    {
+        private string mstrNombre;
+        public string Nombre
+        {
+            get { return mstrNombre; }
+            set { mstrNombre = value; }
+        }
+
+        private object mobjValor;
+        public object Valor
+        {
+            get { return mobjValor; }
+            set { mobjValor = value; }
+        }
+
+        public Parametro()
+        {
+            mstrNombre = "";
+            mobjValor = "";
+        }
+
+        public Parametro(string pstrNombre, object pobjValor)
+        {
+            mstrNombre = pstrNombre;
+            mobjValor = pobjValor;
+        }
+
+    }
+}
